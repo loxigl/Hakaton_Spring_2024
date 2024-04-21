e443075c7d81f4fc8a2f9b5728279e6e064d5687
loxigl <43906792+loxigl@users.noreply.github.com>
2024-04-21 14:48:24 +0300

    fixup: partners logic

--- front/router.py
+++ front/router.py
@@ -36,13 +36,29 @@ def is_weekend():
     return today == 5 or today == 6
 
 
+from datetime import datetime, timedelta
+
+
+def get_next_weekend():
+    today = datetime.today()
+
+    day_of_week = today.weekday()
+    if day_of_week < 5:
+        next_saturday = today + timedelta(days=(5 - day_of_week))
+        return next_saturday.strftime("%d.%m.%y")
+    elif day_of_week == 5 or day_of_week == 6:
+        return today.strftime("%d.%m.%y")
+
+
 @router.get('/partners')
 def partners(request: Request, current_user=Depends(get_current_user_safe), db=Depends(get_db)):
     partner = []
     if not current_user:
         return RedirectResponse(url="/api/v1/login")
-    if (is_weekend()):
+    if not current_user.worksheet:
+        return RedirectResponse(url="/api/v1/worksheet")
+    if is_weekend():
         partner = get_users_with_similar_meetings(current_user=current_user, db=db)
     return templates.TemplateResponse("/partners/base.html",
                                       {"request": request, "current_user": current_user, 'today': is_weekend(),
-                                       'partner': partner[0]})
+                                       'partner': partner, 'next_weekend': get_next_weekend()})
